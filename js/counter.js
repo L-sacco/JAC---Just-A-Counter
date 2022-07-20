485743434dc20310d1fb53c47952b00cae70a9ec
Luigi Sacco <luigi.sa94@gmail.com>
2022-07-20 15:19:29 +0200

    JS Adjustments

--- js/counter.js
+++ js/counter.js
@@ -1,8 +1,9 @@
 const Counter = () => {
     const container = document.querySelector('.container')
-    const   subtitle = document.querySelector(`#subtitle`),
-    add = document.querySelector(`#add`),
-    remove = document.querySelector(`#remove`)
+    const display = document.querySelector('#display')
+    const subtitle = document.querySelector(`#subtitle`),
+    addButton = document.querySelector(`#add`),
+    removeButton = document.querySelector(`#remove`)
     var counter = 0
     document.addEventListener('click', (e) => {
         if (e.target.matches('button')) {
@@ -15,30 +16,30 @@ const Counter = () => {
     
             display.textContent = counter
     
-            if (counter >= 0 && counter < 9) {
-                add.style.visibility = "visible"
-                remove.style.visibility = "visible"
+            if (counter >= 0 && counter < 10) {
+                addButton.style.visibility = "visible"
+                removeButton.style.visibility = "visible"
                 subtitle.textContent = "There you go!"
             }
-            if (counter > 9) subtitle.textContent = "Ok you can stop now."
-            if (counter > 49) subtitle.textContent = "Are you serious? ..."
-            if (counter > 99) subtitle.textContent = "Just stop!"
-            if (counter > 149) subtitle.textContent = "Ok then, keep clicking, i don't care..."
-            if (counter > 199) subtitle.textContent = "I said i don't care... You're wasting your time..."
-            if (counter > 249) subtitle.textContent = "It's enough! Last warning! STOP CLICKING!"
-            if (counter > 299 ) {
+            if (counter == 10) subtitle.textContent = "Ok you can stop now."
+            if (counter == 50) subtitle.textContent = "Are you serious? ..."
+            if (counter == 100) subtitle.textContent = "Just stop!"
+            if (counter == 130) subtitle.textContent = "Ok then, keep clicking, i don't care..."
+            if (counter == 170) subtitle.textContent = "I said i don't care... You're wasting your time..."
+            if (counter == 200) subtitle.textContent = "It's enough! Last warning! STOP CLICKING!"
+            if (counter == 230) {
                 subtitle.textContent = "Is there an end?"
                 add.textContent = "Reset"
                 remove.textContent = "Reset"
                 container.style.flexDirection = "column-reverse"
             }
-            if (counter > 319) {
+            if (counter == 280) {
                 subtitle.textContent = "Ok, i give up! You won... :("
                 container.style.flexDirection = "column"
                 add.textContent = "+1"
                 remove.textContent = "-1"
             }
-            if (counter > 332) {
+            if (counter == 300) {
                 subtitle.textContent = "Just joking, i won! :)"
                 add.style.visibility = "hidden"
                 remove.style.visibility = "hidden"
